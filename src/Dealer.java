8a6df2b5bea12a68c87da262acd55e9743abd083
Raman Mohammed <a21rammo@outlook.com>
2022-04-15 23:12:59 +0200

    added dealer class

---
+++ src/Dealer.java
@@ -0,0 +1,17 @@
+
+public class Dealer {
+
+	private int mScore = 0;
+
+	public Dealer() {
+	}
+
+	public int getScore() {
+		return mScore;
+	}
+
+	public void setScore(int Score) {
+		this.mScore += Score;
+	}
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,42 @@
+public class Player implements PlayerInterface {
+	private String mName;
+	private int mScore;
+	private int mBalance;
 
-public class Player {
+	public Player(String Name, int Balance) {
+		this.mName = Name;
+		this.mBalance = Balance;
+	}
+
+	@Override
+	public String getName() {
+
+		return mName;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.mName = name;
+	}
+
+	@Override
+	public int getScore() {
+		return mScore;
+	}
+
+	@Override
+	public void setScore(int score) {
+		this.mScore += score;
+	}
+
+	@Override
+	public int getBalance() {
+		return mBalance;
+	}
+
+	@Override
+	public void setBalance(int balance) {
+		this.mBalance = balance;
+	}
 
 }
