dbabdea2e3071ddb6dd2b1a9c14f5eab4a0bb3dc
Raman Mohammed <a21rammo@outlook.com>
2022-04-15 23:06:10 +0200

    added player with a playerinterface

---
+++ src/PlayerInterface.java
@@ -0,0 +1,14 @@
+public interface PlayerInterface {
+	public String getName();
+
+	public void setName(String name);
+
+	public int getScore();
+
+	public void setScore(int score);
+
+	public int getBalance();
+
+	public void setBalance(int balance);
+
+}
