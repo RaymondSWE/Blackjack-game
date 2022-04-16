8e2238dd99c260460c28f3895d4de89019245303
Raman Mohammed <a21rammo@outlook.com>
2022-04-16 03:59:24 +0200

    Had wrong path to background and cards caused error, now fixed. But lots of problem with the logic part of application

--- src/Deck.java
+++ src/Deck.java
@@ -1,42 +1,64 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
+import java.util.HashMap;
 
 public class Deck {
+    private final ArrayList<Card> mCards;
+    private Image backCover;
 
-	private ArrayList<Card> mCards = new ArrayList<Card>();
-	private String[] mSuit = { "spades", "diamonds", "clubs", "hearts" };
-	private String[] mRank = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
-	private int[] mValue = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 };
+    public enum cards {
+        Spades, Diamonds, Clubs, Hearts;
+        HashMap<String, Integer> ranks;
 
-	public Deck() {
-		// we create our deck here. The constructor does not expect any input.
-		for (int i = 0; i < mSuit.length; i++) {
-			for (int j = 0; j < mRank.length; j++) {
-				if (mSuit[i].equals("spades"))
-					mCards.add(new Card(mRank[j], mSuit[i], mValue[j]));
-				else if (mSuit[i].equals("hearts"))
-					mCards.add(new Card(mRank[j], mSuit[i], mValue[j]));
-				else if (mSuit[i].equals("clubs"))
-					mCards.add(new Card(mRank[j], mSuit[i], mValue[j]));
-				else if (mSuit[i].equals("diamonds"))
-					mCards.add(new Card(mRank[j], mSuit[i], mValue[j]));
+        cards() {
+            ranks = new HashMap<>();
+            ranks.put("2", 2);
+            ranks.put("3", 3);
+            ranks.put("4", 4);
+            ranks.put("5", 5);
+            ranks.put("6", 6);
+            ranks.put("7", 7);
+            ranks.put("8", 8);
+            ranks.put("9", 9);
+            ranks.put("10", 10);
+            ranks.put("JACK", 10);
+            ranks.put("QUEEN", 10);
+            ranks.put("KING", 10);
+            ranks.put("ACE", 11);
+        }
+    }
 
-			}
-		}
+    public Deck() {
+        mCards = new ArrayList<>();
+        for (cards card : cards.values())
+            for (String rank : card.ranks.keySet())
+                mCards.add(new Card(card.toString(), rank, card.ranks.get(rank)));
+        loadImages();
+    }
 
-	}
+    public void loadImages() {
+        try {
+            for (Card card : mCards)
+                card.setImage(ImageIO.read(new File(card.getFullImagePath())).getScaledInstance(50, 73, BufferedImage.SCALE_SMOOTH));
+            backCover = ImageIO.read(new File("Images\\Cardimages\\BACK COVER.png")).getScaledInstance(50, 73, BufferedImage.SCALE_SMOOTH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	// this function takes no input, returns void and shuffles the deck.
-	public void shuffleDeck() {
-		Collections.shuffle(mCards);
-	}
+    public void shuffleDeck() {
+        Collections.shuffle(mCards);
+    }
 
-	// this function returns an array of Card objects containing 3 cards from the
-	// deck.
-	public Card getPlayerCards(int index) {
-		// the size of the array below will be 3 since the player is delt 3 cards.
-		Card playerCard = mCards.get(index);
-		return playerCard;
-	}
+    public Card getPlayerCards(int index) {
+        return mCards.get(index = index % mCards.size());
+    }
+
+    public Image getBackCover() {
+        return backCover;
+    }
 }
